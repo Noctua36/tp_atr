5e34306599202b8620f4319516055c6b4a470710
Gustavo O. Mendonça <gustavo36@gmail.com>
2019-06-22 20:10:34 -0300

    Implementado algoritmo Runge-Kutta para a thread de processo

--- src/app.py
+++ src/app.py
@@ -2,15 +2,12 @@ from threading import Thread
 # import threading
 # import socket
 from parametros_tanque import parametros
-from math import sqrt as raiz, pi
+from math import sqrt, pi
 import logging
 import time
 
 
 logging.basicConfig(level=logging.DEBUG)
-
-
-global vazao_in, vazao_out, nivel_atual, nivel_ref
 
 vazao_in = 0.0
 nivel_atual = 8.0
@@ -22,21 +19,35 @@ class ProcessThread(Thread):
     def __init__(self, cv, raio_inf, raio_sup, altura):
         ''' Constructor. '''
         Thread.__init__(self)
-        self.Cv = cv # Coeficiente de descarga do tanque -> relacionado com o tamanho do furo no tanque
+        self.cv = cv # Coeficiente de descarga do tanque -> relacionado com o tamanho do furo no tanque
         self.raio_inf = raio_inf # Raio inferior do tanque
         self.raio_sup = raio_sup # Raio superior do tanque
         self.altura_tanque = altura
+        self.alfa = (raio_sup-raio_inf)/altura
     
+    @staticmethod
+    def dhdt(self, t, h, u):
+        return (-self.cv*sqrt(h)+u)/pi*(self.raio_inf+self.alfa*h)**2 if h>0 else 0
+
+    # Runge-Kutta
+    @staticmethod
+    def rk4(self, f, t0, h0, dt, u):
+        f1 = f(self, t0, h0, u)
+        f2 = f(self, t0+dt/2.0, h0+dt*f1/2.0, u)
+        f3 = f(self, t0+dt/2.0, h0+dt*f2/2.0, u)
+        f4 = f(self, t0+dt, h0+dt*f3, u)
+        return h0+dt*(f1+2.0*f2+2.0*f3+f4)/6.0
+
     def run(self):
+        global vazao_in, vazao_out, nivel_atual, nivel_ref
+        lastT = time.time()
         logging.debug('Process Thread iniciada')
         while True:
-            global vazao_in, vazao_out, nivel_atual, nivel_ref
-            alfa = (self.raio_sup-self.raio_inf)/self.altura_tanque
-            vazao_out = self.Cv * raiz(nivel_atual)
-            denom = pi*(self.raio_inf+alfa*nivel_atual)**2
-            variacao_nivel = -vazao_out/denom + vazao_in/denom
-            nivel_atual += variacao_nivel
-            logging.debug('nivel_atual = {} \n'.format(nivel_atual))
+            now = time.time()
+            deltaT = now - lastT
+            lastT = now
+            nivel_atual = self.rk4(self, self.dhdt, lastT, nivel_atual, deltaT, vazao_in)
+            logging.debug('nivel_atual = {}'.format(nivel_atual))
             time.sleep(0.050)
             
 
@@ -84,11 +95,13 @@ class Executor():
     def run(self):
         processThread = ProcessThread(parametros['Cv'], parametros['raio_inf'], parametros['raio_sup'], parametros['altura'])
         processThread.setDaemon(True)
+        processThread.setName('processThread')
         processThread.start()
         processThread.join()
 
         softPLCThread = SoftPLCThread()
         softPLCThread.setDaemon(True)
+        processThread.setName('softPLCThread')
         softPLCThread.start()
         softPLCThread.join()
 
