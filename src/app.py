6bd14df0acf493ad188f7a827b0ae3fd996fc2eb
Gustavo O. Mendonça <gustavo36@gmail.com>
2019-06-22 23:32:44 -0300

    Implementação do recebimento dos dados pelo sinóptico

--- src/app.py
+++ src/app.py
@@ -1,100 +1,144 @@
 from threading import Thread
-# import threading
-# import socket
+from socket import AF_INET, socket, SOCK_STREAM
 from parametros_tanque import parametros
 from math import sqrt, pi
 import logging
 import time
 from subprocess import call
+# TODO: criar implementação pŕopria do PID
 from simple_pid import PID
 
 logging.basicConfig(level=logging.DEBUG)
 
 vazao_in = 0.0
 nivel_atual = 8.0
-nivel_ref = 10.0 # setpoint
+nivel_ref = 10.0  # setpoint
 vazao_out = 0.0
 
-
 pid = PID(1, 0.1, 0.05, setpoint=nivel_ref)
-pid.output_limits = (0, 10) 
+pid.output_limits = (0, 10)
 pid.tunings = (1.0, 0.2, 0.4)
 
+
 class ProcessThread(Thread):
-    
     def __init__(self, cv, raio_inf, raio_sup, altura):
-        ''' Constructor. '''
         Thread.__init__(self)
-        self.cv = cv # Coeficiente de descarga do tanque -> relacionado com o tamanho do furo no tanque
-        self.raio_inf = raio_inf # Raio inferior do tanque
-        self.raio_sup = raio_sup # Raio superior do tanque
+        self.cv = (
+            cv
+        )  # Coeficiente de descarga do tanque -> relacionado com o tamanho do furo no tanque
+        self.raio_inf = raio_inf  # Raio inferior do tanque
+        self.raio_sup = raio_sup  # Raio superior do tanque
         self.altura_tanque = altura
-        self.alfa = (raio_sup-raio_inf)/altura
-    
+        self.alfa = (raio_sup - raio_inf) / altura
+
     @staticmethod
     def dhdt(self, t, h, u):
-        return (-self.cv*sqrt(h)+u)/pi*(self.raio_inf+self.alfa*h)**2 if h>0 else 0
+        return (
+            (-self.cv * sqrt(h) + u) / pi * (self.raio_inf + self.alfa * h) ** 2
+            if h > 0
+            else 0
+        )
 
     # Runge-Kutta
     @staticmethod
     def rk4(self, f, t0, h0, dt, u):
         f1 = f(self, t0, h0, u)
-        f2 = f(self, t0+dt/2.0, h0+dt*f1/2.0, u)
-        f3 = f(self, t0+dt/2.0, h0+dt*f2/2.0, u)
-        f4 = f(self, t0+dt, h0+dt*f3, u)
-        return h0+dt*(f1+2.0*f2+2.0*f3+f4)/6.0
+        f2 = f(self, t0 + dt / 2.0, h0 + dt * f1 / 2.0, u)
+        f3 = f(self, t0 + dt / 2.0, h0 + dt * f2 / 2.0, u)
+        f4 = f(self, t0 + dt, h0 + dt * f3, u)
+        return h0 + dt * (f1 + 2.0 * f2 + 2.0 * f3 + f4) / 6.0
 
     def run(self):
-        global vazao_in, nivel_atual
+        global vazao_in, vazao_out, nivel_atual
         lastT = time.time()
-        logging.debug('Process Thread iniciada')
+        logging.debug("Process Thread iniciada")
         while True:
             now = time.time()
             deltaT = now - lastT
             lastT = now
-            nivel_atual = self.rk4(self, self.dhdt, lastT, nivel_atual, deltaT, vazao_in)
-            logging.debug('nivel_atual = {:.2f}'.format(nivel_atual))
+            nivel_atual = self.rk4(
+                self, self.dhdt, lastT, nivel_atual, deltaT, vazao_in
+            )
+            #logging.debug('nivel_atual = {:.2f}'.format(nivel_atual))
+            vazao_out = self.cv * sqrt(nivel_atual)
             time.sleep(0.05)
-            
+
 
 class SoftPLCThread(Thread):
     def __init__(self):
-        ''' Constructor. '''
         Thread.__init__(self)
-        
+
     def run(self):
-        logging.debug('Soft PLC Thread iniciada')
-        global vazao_in, nivel_atual #, nivel_ref
+        logging.debug("Soft PLC Thread iniciada")
+        # TODO threading.lock()
+        global vazao_in, nivel_atual
         while True:
             vazao_in = pid(nivel_atual)
-            # if nivel_atual > nivel_ref:
-            #     vazao_in *= 0.9 # diminui 5% da vazao
-            # elif nivel_atual < nivel_ref:
-            #     vazao_in = vazao_in*1.15 if vazao_in>0 else 0.5 # aumenta 5% da vazao
-
-            logging.debug('vazao_in = {:.2f}'.format(vazao_in))
+            #logging.debug('vazao_in = {:.2f}'.format(vazao_in))
             time.sleep(0.05)
 
-        
-class Executor():
+
+class ServerThread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.host = "0.0.0.0"
+        self.port = 30000
+        self.addr = (self.host, self.port)
+        self.bufSize = 1024
+        self.server = socket(AF_INET, SOCK_STREAM)
+        self.server.bind(self.addr)
 
     def run(self):
+        logging.debug('Server Thread iniciada')
         
+        # self.server.bind(self.addr)
+        self.server.listen()
+        logging.debug('Aguardando conexão do sinóptico')
+        conn, cliAddr = self.server.accept()
+
+        # logging.debug('Connectado por' + cliAddr)
+        while True:
+            # conn.send(b'123')#bytes(nivel_atual, "utf8"))
+            conn.send('\nnivel_atual: {:.2f}\nsetpoint: {:.2f}\nvazao_in: {:.2f}\nvazao_out: {:.2f}\n'.format(nivel_atual, nivel_ref, vazao_in, vazao_out).encode())
+                # str(nivel_atual).encode())
+            time.sleep(1)
+
+            # data = conn.recv(self.bufSize)
+            # if not data:
+            #     print('sem dados')
+            
+
+
+class Executor:
+    def run(self):
+
         softPLCThread = SoftPLCThread()
         softPLCThread.setDaemon(True)
-        softPLCThread.setName('softPLCThread')
+        softPLCThread.setName("softPLCThread")
         softPLCThread.start()
 
-        processThread = ProcessThread(parametros['Cv'], parametros['raio_inf'], parametros['raio_sup'], parametros['altura'])
+        processThread = ProcessThread(
+            parametros["Cv"],
+            parametros["raio_inf"],
+            parametros["raio_sup"],
+            parametros["altura"],
+        )
         processThread.setDaemon(True)
-        processThread.setName('processThread')
+        processThread.setName("processThread")
         processThread.start()
 
-        call(['python', 'synoptic.py'])
+        serverThread = ServerThread()
+        serverThread.setDaemon(True)
+        serverThread.setName("serverThread")
+        serverThread.start()
+
+        time.sleep(2)
+        call(["python", "synoptic.py"])
 
         softPLCThread.join()
         processThread.join()
-       
+        serverThread.join()
+
 
 main = Executor()
 main.run()
--- src/synoptic.py
+++ src/synoptic.py
@@ -1,1 +1,86 @@
-print('HELLO')
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import logging
+import time
+import random
+
+logging.basicConfig(level=logging.DEBUG)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+bufSize = 1024
+
+
+class ReceiveThread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        logging.debug("Receive Thread iniciada")
+        global client_socket
+        while True:
+            try:
+                msg = client_socket.recv(bufSize).decode("utf8")
+                if not msg: break
+                print("mensagem recebida: %s" % msg)
+            except OSError: 
+                pass
+                # break
+
+
+class SendThread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        logging.debug("Send Thread iniciada")
+        global client_socket
+        while True:
+            #TODO: ler entrada do usuário
+            setpoint = round(random.uniform(0, 10), 2)
+            try:
+                client_socket.send(bytes(str(setpoint), "utf8"))
+                if setpoint == "exit":
+                    client_socket.close()
+            except OSError:
+                pass
+                # break
+
+            time.sleep(30)
+
+
+class Synoptic:
+    
+    def __init__(self):
+        Thread.__init__(self)
+        self.host = "127.0.0.1"
+        self.port = 30000
+        self.addr = (self.host, self.port)
+        
+    def run(self):
+        
+        client_socket.connect(self.addr)
+        while True:
+            
+            # try:
+            msg = client_socket.recv(bufSize).decode()
+            # if not msg: break
+            logging.debug( msg)
+            # except OSError: 
+            #     logging.debug('erro')
+
+        # receiveThread = ReceiveThread()
+        # receiveThread.setDaemon(True)
+        # receiveThread.setName("receiveThread")
+        # receiveThread.start()
+
+        # sendThread = SendThread()
+        # sendThread.setDaemon(True)
+        # sendThread.setName("SendThread")
+        # sendThread.start()
+
+        # sendThread.join()
+        # receiveThread.join()
+
+
+main = Synoptic()
+main.run()
